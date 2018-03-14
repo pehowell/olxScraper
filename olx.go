d28678b5c408fb4ec1651cf6dc03064470fa8c9f
Paul Howell <paul.howell@gmail.com>
2018-03-14 12:03:37 +0800

    Added docopt; Capture link to item; minor cleanup

--- olx.go
+++ olx.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/anaskhan96/soup"
+	"github.com/docopt/docopt-go"
 	"io/ioutil"
 	"os"
 	"regexp"
@@ -14,6 +15,7 @@ import (
 type Item struct {
 	Name  string `json:"name"`
 	Price int    `json:"price"`
+	Link string `json:"link"`
 }
 
 func check(e error) {
@@ -25,8 +27,8 @@ func check(e error) {
 func loadItems(filename string) []Item {
 	var items []Item
 	if _, err := os.Stat(filename); err == nil {
-		data, error := ioutil.ReadFile(filename)
-		check(error)
+		data, readErr := ioutil.ReadFile(filename)
+		check(readErr)
 		json.Unmarshal(data, &items)
 	}
 	return items
@@ -35,30 +37,54 @@ func loadItems(filename string) []Item {
 func saveItems(items []Item, filename string) {
 	data, err := json.Marshal(&items)
 	check(err)
-	error := ioutil.WriteFile(filename, data, 0644)
-	check(error)
+	writeErr := ioutil.WriteFile(filename, data, 0644)
+	check(writeErr)
 }
 
 func main() {
-	if len(os.Args) != 2 {
-		panic("Must provide json file")
-	}
-	filename := os.Args[1]
+	usage := `Olx.ph Scraper
+
+Usage:
+  olx [-hp] <pattern> <jsonfile>
+
+Options:
+  -h, --help     Show this screen.
+  -p, --print    Print new items.`
+
+	args, _ := docopt.ParseDoc(usage)
+  printNew, _ := args.Bool("--print")
+  pattern, _ := args.String("<pattern>")
+  filename, _ := args.String("<jsonfile>")
+
+	olxURL := "https://www.olx.ph"
+
+	var searchURL strings.Builder
+	searchURL.WriteString(olxURL)
+	searchURL.WriteString("/all-results?q=")
+  searchURL.WriteString(pattern)
+
 	items := loadItems(filename)
 	var foundItems []Item
 	var newItems []Item
 	re, _ := regexp.Compile("[0-9]+")
-	resp, err := soup.Get("https://www.olx.ph/all-results?q=ags-101")
+	resp, err := soup.Get(searchURL.String())
 	check(err)
 
 	doc := soup.HTMLParse(resp)
 	results := doc.FindAll("div", "itemid", "#product")
 	for _, result := range results {
+    var itemURL strings.Builder
+    link := result.Find("a", "itemprop", "url")
+    itemURL.WriteString(olxURL)
+    itemURL.WriteString(link.Attrs()["href"])
+
 		name := result.Find("span", "itemprop", "name")
+
 		priceLine := result.Find("div", "itemprop", "offers").Find("span", "class", "price")
 		price, err := strconv.Atoi(re.FindString(strings.Replace(priceLine.Text(), ",", "", -1)))
 		check(err)
-		foundItems = append(foundItems, Item{name.Text(), price})
+
+		foundItems = append(foundItems, Item{name.Text(), price, itemURL.String()})
 	}
 	for _, foundItem := range foundItems {
 		known := false
@@ -72,7 +98,7 @@ func main() {
 		}
 	}
 	saveItems(foundItems, filename)
-	if len(newItems) > 0 {
+	if printNew && len(newItems) > 0 {
 		for _, item := range newItems {
 			fmt.Printf("%v -> %v\n", item.Name, item.Price)
 		}
